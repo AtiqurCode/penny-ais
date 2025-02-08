a0ab1aa4ddf17a6e618e5e565a5a10752dbc7cd3
AtiqurCode <atqur9@gmail.com>
2025-02-08 19:08:30 +0600

    create a account when a new user create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Team;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Account;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -25,19 +27,37 @@ class UserController extends Controller
         // Prepare additional fields
         $request->merge([
             'password' => bcrypt("password"), // Default password
-            'email_verified_at' => now(), // Mark email as verified
+            'email_verified_at' => now(), // Mark the user as verified
         ]);
 
-        // Create the user
-        $user = User::create($request->all());
+        DB::beginTransaction();
 
-        // Create the user's team
-        Team::create([
-            'user_id' => $user->id, // Associate the team with the newly created user
-            'name' => $user->name . "'s Team",
-            'personal_team' => true,
-        ]);
+        try {
+            // Create the user
+            $user = User::create($request->all());
 
+            // Create the user's team
+            Team::create([
+                'user_id' => $user->id, // Associate the team with the newly created user
+                'name' => $user->name . "'s Team",
+                'personal_team' => true,
+            ]);
+
+            // create the user account
+            Account::create([
+                'user_id' => $user->id,
+                'name' => $user->name,
+                'type' => 'expense',
+                'balance' => 0,
+            ]);
+
+            DB::commit();
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Failed to create user and team');
+        }
+        
         // Redirect to the users listing page
         return redirect()->route('users')->with('success', 'User and team created successfully!');
     }
